8dbbf05db626aa8074d72677d5ada57793631c36
muhammad.azis <muhammad.azis@pji-solutions.com>
2018-01-03 12:59:02 +0700

    add login process

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,11 +5,77 @@
  * Date: 16/12/2017
  * Time: 22.58
  */
+session_start();
 
 class Auth extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+
+		//Load form helper library
+		$this->load->helper('form');
+
+		//Load form validation library
+		$this->load->library('form_validation');
+
+		//Load session library
+		$this->load->library('session');
+
+		//Load database
+		$this->load->model('m_auth');
+	}
+
 	public function login() {
 		$this->load->view('layout/common/login');
+	}
+
+	public function login_process() {
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
+
+		if ($this->form_validation->run() == FALSE) {
+			if (isset($this->session->userdata['logged_in'])) {
+				//$this->load->view('layout/layout');
+				redirect('home', 'refresh');
+			} else {
+				redirect('auth/login', 'refresh');
+			}
+		} else {
+			$data = array(
+				'username' => $this->input->post('username'),
+				'password' => $this->input->post('password')
+			);
+			$result = $this->m_auth->login($data);
+			if ($result == TRUE) {
+				$username = $this->input->post('username');
+				$result = $this->m_auth->read_user_information($username);
+				if ($result != FALSE) {
+					$session_data = array(
+						'username' => $result[0]->username,
+						'email' => $result[0]->user_mail
+					);
+					//Add user data in session
+					$this->session->set_userdata('logged_in', $session_data);
+					$this->load->view('layout/layout');
+				}
+			} else {
+				$data = array(
+					'error_message' => 'Invalid username or password'
+				);
+				$this->load->view('layout/common/login', $data);
+			}
+		}
+	}
+
+	//Logout
+	public function logout() {
+		//Removing session data
+		$session_array = array(
+			'username' => ''
+		);
+		$this->session->unset_userdata('logged_in', $session_array);
+		$data['message_display'] = 'Successfully Logout';
+		$this->load->view('layout/common/login', $data);
 	}
 
 	public function go() {
